6be445f8a527d2119be9469bdba8a5b4fad71be4
bushuev-alex <bushuev-alex@mail.ru>
2022-01-13 13:39:15 +0700

    In each error function add self.append(code_no and path) to be able to sort by path, lineno and error_code. Change errors.sort to get right result.

--- task/analyzer/code_analyzer.py
+++ task/analyzer/code_analyzer.py
@@ -33,35 +33,35 @@ class Checker:
 
     def error_s001(self, m, m_line):  # 'Too long'
         if len(m_line) > 79:
-            self.errors.append([m, f"{self.path}: Line {m}: S001 {self.codes['S001']}"])
+            self.errors.append([m, 'S001', self.path, f"{self.path}: Line {m}: S001 {self.codes['S001']}"])
 
     def error_s002(self, m, m_line):  # Indentation is not a multiple of four
         if len(re.match('^ *', m_line)[0]) % 4 != 0:
-            self.errors.append([m, f"{self.path}: Line {m}: S002 {self.codes['S002']}"])
+            self.errors.append([m, 'S002', self.path, f"{self.path}: Line {m}: S002 {self.codes['S002']}"])
 
     def error_s003(self, m, m_line):  # Unnecessary semicolon after a statement
         if ";" in m_line:
             if re.match(".*#.*;.*", m_line):
                 return True
             elif re.match(""".*('|").*;.*('|").*""", m_line) is None:
-                self.errors.append([m, f"{self.path}: Line {m}: S003 {self.codes['S003']}"])
+                self.errors.append([m, 'S003', self.path, f"{self.path}: Line {m}: S003 {self.codes['S003']}"])
 
     def error_s004(self, m, m_line):  # Less than two spaces before inline comments
         if '#' in m_line:
             if re.match("^#.*", m_line):
                 pass
             elif re.match('.*( ){2,}#.*', m_line) is None and '#' in m_line:
-                self.errors.append([m, f"{self.path}: Line {m}: S004 {self.codes['S004']}"])
+                self.errors.append([m, 'S004', self.path, f"{self.path}: Line {m}: S004 {self.codes['S004']}"])
 
     def error_s005(self, m, m_line):  # TO_DO found (in comments only and case-insensitive)
         if re.match('.*#.*TODO.*', m_line, re.IGNORECASE):
-            self.errors.append([m, f"{self.path}: Line {m}: S005 {self.codes['S005']}"])
+            self.errors.append([m, 'S005', self.path, f"{self.path}: Line {m}: S005 {self.codes['S005']}"])
 
     def error_s006(self, m, m_line):  # More than two blank lines preceding a code line
         if m_line.strip() == '':
             self.blank_line_count += 1
             if self.blank_line_count > 2:
-                self.errors.append([m+1, f"{self.path}: Line {m+1}: S006 {self.codes['S006']}"])
+                self.errors.append([m+1, 'S006', self.path, f"{self.path}: Line {m+1}: S006 {self.codes['S006']}"])
                 self.blank_line_count = 0
 
     def error_s007(self, m, m_line):  # Too many spaces after construction_name (def or class)
@@ -71,7 +71,7 @@ class Checker:
                 name = m_line[len(beginning_line):-2]
             elif 'def' in beginning_line:
                 name = m_line[len(beginning_line):-4]
-            self.errors.append([m, f"{self.path}: Line {m}: S007 {self.codes['S007'].format(name)}"])
+            self.errors.append([m, 'S007', self.path, f"{self.path}: Line {m}: S007 {self.codes['S007'].format(name)}"])
 
     def error_s008(self, m, m_line):  # Class name {} should be written in CamelCase
         if 'class' in m_line:
@@ -79,7 +79,7 @@ class Checker:
                 if re.match(".*class [a-z]+", m_line):
                     beginning_line = re.match(".*class +[A-Z]?", m_line)[0]
                     class_name = m_line[len(beginning_line):-2]
-                    self.errors.append([m, f"{self.path}: Line {m}: S008 {self.codes['S008'].format(class_name)}"])
+                    self.errors.append([m, 'S008', self.path, f"{self.path}: Line {m}: S008 {self.codes['S008'].format(class_name)}"])
 
     def error_s009(self, m, m_line):  # Function name {} should be written in snake_case
         if 'def' in m_line:
@@ -87,10 +87,12 @@ class Checker:
                 if re.match(".*def +[A-Z]", m_line):
                     beginning_line = re.match(".*def +[a-z]?", m_line)[0]
                     def_name = m_line[len(beginning_line):-4]
-                    self.errors.append([m, f"{self.path}: Line {m}: S009 {self.codes['S009'].format(def_name)}"])
+                    self.errors.append([m, 'S009', self.path, f"{self.path}: Line {m}: S009 {self.codes['S009'].format(def_name)}"])
 
     def check_file(self):
         if re.match('.*\\.py$', self.path):
+
+            # Line-by-line checkers
             with open(self.path, 'r', encoding="UTF-8") as file:
                 n = 1
                 for line in file:
@@ -106,7 +108,7 @@ class Checker:
                     self.error_s009(n, line)  # Function name {} should be written in snake_case
                     n += 1
 
-            # AST checkers
+            # AST node checkers
             with open(self.path, 'r', encoding="UTF-8") as file:
                 text = file.read()
                 tree = ast.parse(text)
@@ -118,7 +120,7 @@ class Checker:
                             lineno = func['lineno']
                             arg_name = func['arg']
                             if not re.match("^[a-z0-9_]*_?[a-z0-9]*$", arg_name):
-                                self.errors.append([lineno, f"{self.path.lower()}: Line {lineno}: S010 {self.codes['S010'].format(arg_name)}"])  #
+                                self.errors.append([lineno, 'S010', self.path, f"{self.path.lower()}: Line {lineno}: S010 {self.codes['S010'].format(arg_name)}"])  #
                         for obj in node.body:  # Variable {} should be written in snake_case
                             if isinstance(obj, ast.Assign):
                                 for target in obj.targets:
@@ -130,19 +132,21 @@ class Checker:
                                         var_name = var_dict['value'].id + '.' + var_dict['attr']
                                         lineno = var_dict['lineno']
                                     if not re.match("^[a-z0-9_]*\.?_?[a-z0-9]*$", var_name):
-                                        self.errors.append([lineno, f"{self.path}: Line {lineno}: S011 {self.codes['S011'].format(var_name)}"])
+                                        self.errors.append([lineno, 'S011', self.path, f"{self.path}: Line {lineno}: S011 {self.codes['S011'].format(var_name)}"])
                         for default in node.args.defaults:  # The default argument value is mutable
-                            if isinstance(default, ast.List) or isinstance(default, ast.Dict) \
-                                    or isinstance(default, ast.Set):
+                            if not isinstance(default, ast.Constant):
                                 default_dict = default.__dict__
                                 lineno = default_dict['lineno']
-                                self.errors.append([lineno, f"{self.path}: Line {lineno}: S012 {self.codes['S012']}"])
+                                self.errors.append([lineno, 'S012', self.path, f"{self.path}: Line {lineno}: S012 {self.codes['S012']}"])
 
     def check_dir(self):
         file_list = os.listdir(self.path)
+        file_list.sort()
+        # print(file_list)
         for file in file_list:
             path = self.path
             self.path = self.path + f'{os.sep}' + file
+            # print(self.path)
             self.check_file()
             self.path = path
 
@@ -153,9 +157,9 @@ class Checker:
             return "file"
 
     def print_errors(self):
-        self.errors.sort()
+        self.errors.sort(key=lambda element: (element[2], element[0], element[1]))
         for error in self.errors:
-            print(error[1].strip().lower())
+            print(error[3])
 
 
 my_checker = Checker(params)
